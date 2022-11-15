6863e4118d4e6090a2a2aa801c30969c26fdcac6
Alexei <i0ver@mail.ru>
2022-11-15 15:18:23 +0300

    Updates

---
+++ precache-manifest.267d7c106d351c4551eb008e03ab1c54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6bcc73124d8b2c1c3ebf332aa438259",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "3c9cb2282f3020e90a3a",
+    "url": "/todolist/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "1129bcbf4dc65527af4f",
+    "url": "/todolist/static/js/2.eb4ca22e.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist/static/js/2.eb4ca22e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c9cb2282f3020e90a3a",
+    "url": "/todolist/static/js/main.eda8fc4e.chunk.js"
+  },
+  {
+    "revision": "c3d5ccae1ee3a7bf91ff",
+    "url": "/todolist/static/js/runtime-main.16223656.js"
+  }
+]);
